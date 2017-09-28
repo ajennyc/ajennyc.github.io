91ceaba938254a9a92e76abb26ecb5a66063da02
jenny <stoby.jenny@gmail.com>
2017-09-28 16:02:45 +0200

    Game is ready
    Finished game - lacks only extras.

---
+++ source/gameoverstate.js
@@ -0,0 +1,25 @@
+var gameOverState = {}; 
+
+gameOverState.preload = function () {
+    
+};
+
+gameOverState.create = function () {
+    var style = {font: "40px Cinzel Decorative", fontWeight: "bold", align: "center"};
+    
+    this.death = game.add.text(game.width *0.5,game.height *0.5,"GAME OVER!", style);
+    this.death.fixedToCamera = true; 
+    this.death.anchor.setTo(0.5,0.5);
+    var grd = this.death.context.createLinearGradient(0,0,0,this.death.height);
+    grd.addColorStop(0, '#e82a04');   
+    grd.addColorStop(1, '#e67958');
+    this.death.fill = grd;
+    
+    this.reStart = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+}; 
+
+gameOverState.update = function () {
+    if (this.reStart.isDown) {
+        game.state.start("MainGame");
+    };
+}; 
--- source/maingamestate.js
+++ source/maingamestate.js
@@ -5,13 +5,21 @@ mainGameState.preload = function () {
     
     game.load.image('sky', 'assets/images/sky.jpg');
     game.load.image('playership', 'assets/images/player-ship.png');
-    game.load.audio('background-music', 'assets/music/maingame.mp3'); 
+    game.load.audio('background-music', 'assets/music/firefly.m4a'); 
     game.load.image('asteroid', 'assets/images/asteroid-medium-01.png');
-    game.load.image('player-bullet', 'assets/images/bullet_fire.png');
+    game.load.image('player-bullet', 'assets/images/bullet-fire.png');
+    game.load.audio('bullet-1', 'assets/audio/player_fire_01.mp3');
+    game.load.audio('bullet-2', 'assets/audio/player_fire_02.mp3');
+    game.load.audio('bullet-3', 'assets/audio/player_fire_03.mp3');
+    game.load.audio('sound-asteroid-1', 'assets/audio/asteroid_hit_01.mp3');
+    game.load.audio('sound-asteroid-2', 'assets/audio/asteroid_hit_02.mp3');
+    game.load.audio('sound-asteroid-3', 'assets/audio/asteroid_hit_03.mp3');
+    game.load.audio('sound-death', 'assets/audio/final_boss_death_01.mp3');
+    game.load.spritesheet('particles','assets/images/asteriod-small-01',9,9);
 
 };
 
-//-----------------------------------------------------------------------
+//---------------------------------------------------------------------
 
 mainGameState.create = function () {
     var x = game.width*0.1;
@@ -24,7 +32,7 @@ mainGameState.create = function () {
     this.playerShip = game.add.sprite(x, y, 'playership');   
     
     this.playerShip.anchor.setTo(0.5,0.5);
-    this.playerShip.scale.setTo(1,1);
+    this.playerShip.scale.setTo(0.8,0.8);
     this.playerShip.angle = -90;
     
 //Movement:
@@ -32,17 +40,81 @@ mainGameState.create = function () {
     game.physics.arcade.enable(this.playerShip);
     this.cursors = game.input.keyboard.createCursorKeys();
     
-//Shooting:
-    this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.Z);
-    this.playerBullets = game.add.group();
-    
-//Music: 
+////Music: 
     this.music = game.add.audio('background-music');
     this.music.play();
+    
+//Sound effects BULLETS:
+    this.playerFireSfx = [];
+    this.playerFireSfx.push(game.add.audio('bullet-1'));
+    this.playerFireSfx.push(game.add.audio('bullet-2'));
+    this.playerFireSfx.push(game.add.audio('bullet-3'));
+    
+//Sound effect ASTEROIDS:
+    this.playerAsteroidSfx = [];
+    this.playerAsteroidSfx.push(game.add.audio('sound-asteroid-1'));
+    this.playerAsteroidSfx.push(game.add.audio('sound-asteroid-2'));
+    this.playerAsteroidSfx.push(game.add.audio('sound-asteroid-3'));
+    
+//Sound effect DEATH: 
+    this.death = game.add.audio('sound-death');
     
 //Astroids: 
     this.asteroidTimer = 2.0;
     this.asteroids = game.add.group();
+    
+//Destroyed astroid:
+//    var emitter = game.add.emitter(this.asteroid.x, this.asteroid.y,100);
+//    emitter.makeParticles('particles', [1,2,3,4,5]);
+//    emitter.minParticleSpeed.setTo(-400,-400);
+//    emitter.maxParticleSpeed.setTo(400,400);
+//    emitter.gravity = 0; 
+    
+//Shooting:
+    this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.Z);
+    this.fireTimer = 0.4;
+    this.playerBullets = game.add.group();
+    
+//Score:
+    this.playerScore = 0; 
+    var style = {font: "16px Cinzel Decorative", fontWeight: "bold", align: "center"};
+    
+    this.scoreTitle = game.add.text(game.width *0.85,30,"Score: ", style);
+    this.scoreTitle.fixedToCamera = true; 
+    this.scoreTitle.anchor.setTo(0.5,0.5);
+    var grdTitle = this.scoreTitle.context.createLinearGradient(0,0,0,this.scoreTitle.height);
+    grdTitle.addColorStop(0, '#e82a04');   
+    grdTitle.addColorStop(1, '#e67958');
+    this.scoreTitle.fill = grdTitle;
+    
+    this.scoreValue = game.add.text(game.width *0.85, 60, "0", style);
+    this.scoreValue.fixedToCamera = true; 
+    this.scoreValue.anchor.setTo(0.5,0.5);
+    var grdValue = this.scoreValue.context.createLinearGradient(0,0,0,this.scoreValue.height);
+    grdValue.addColorStop(0, '#e82a04');   
+    grdValue.addColorStop(1, '#e67958');
+    this.scoreValue.fill = grdValue;
+
+//Life and Death: 
+    this.playerLife = 3; 
+    var style = {font: "16px Cinzel Decorative", fontWeight: "bold", align: "center"};
+    
+    this.lifeTitle = game.add.text(game.width *0.85,game.height *0.85,"Life: ", style);
+    this.lifeTitle.fixedToCamera = true; 
+    this.lifeTitle.anchor.setTo(0.5,0.5);
+    var grdTitle1 = this.scoreTitle.context.createLinearGradient(0,0,0,this.lifeTitle.height);
+    grdTitle1.addColorStop(0, '#e82a04');   
+    grdTitle1.addColorStop(1, '#e67958');
+    this.lifeTitle.fill = grdTitle1;
+    
+    this.lifeValue = game.add.text(game.width *0.85, game.height *0.90, "3", style);
+    this.lifeValue.fixedToCamera = true; 
+    this.lifeValue.anchor.setTo(0.5,0.5);
+    var grdValue1 = this.scoreValue.context.createLinearGradient(0,0,0,this.lifeValue.height);
+    grdValue1.addColorStop(0, '#e82a04');   
+    grdValue1.addColorStop(1, '#e67958');
+    this.lifeValue.fill = grdValue1;
+    
 };
 
 //-----------------------------------------------------------------------
@@ -69,11 +141,10 @@ mainGameState.update = function () {
         this.playerShip.body.velocity.x = -500;
     } else {
         this.playerShip.body.velocity.x = 0;
-}
+    }
     
 //Stop it at the boarders: 
-    
-    if ((this.playerShip.x  > game.width) && (this.playerShip.body.velocity.x > 0)){
+      if ((this.playerShip.x  > game.width) && (this.playerShip.body.velocity.x > 0)){
          this.playerShip.body.velocity.x = 0;   
     }
     if (this.playerShip.x < 0 && this.playerShip.body.velocity.x < 0){
@@ -86,7 +157,7 @@ mainGameState.update = function () {
          this.playerShip.body.velocity.y = 0;   
     }    
     
-// Asteroids: 
+//Asteroids: 
     this.asteroidTimer -= game.time.physicsElapsed;
 
     if (this.asteroidTimer <= 0.0){
@@ -94,24 +165,32 @@ mainGameState.update = function () {
         this.spawnAsteroid();
         this.asteroidTimer = 3.0; 
     }
-    for(var i = 0; i < this.asteroids.children.lenght; i++){
-        if(this.asteroids.children[i].y > (game.width - 200)){
+    for(var i = 0; i < this.asteroids.children.length; i++){
+        if(this.asteroids.children[i].x < - 100){
             this.asteroids.children[i].destroy();       
         }
     }
 
-//Shooting at the asteroids:     
+//Shooting: 
+    this.updatePlayerBullets();
     
-    if(this.fireKey.isDown){
-         console.log("FIREBALLS! :D ");
-        this.spawnPlayerBullet();
-    }
-    for(var i = 9; i < this.playerBullets.children.lenght; i++){
-        if(this.asteriods.children[i].y > (game.width - 200)){
-            this.playerBullets.children[i].destroy();
-        }
-    }
+//Asteroids and bullets colliding: 
+    game.physics.arcade.collide(this.asteroids, this.playerBullets, this.asteroidBulletCollision, null, this);
+
+
+//Astroids and player colliding: 
+    game.physics.arcade.collide(this.playerShip, this.asteroids, this.astroidPlayerCollision, null, this);
     
+//Score:
+    this.scoreValue.setText(this.playerScore);
+    
+//Life:
+    this.lifeValue.setText(this.playerLife);
+
+//Death: 
+    if(this.playerLife < 0){
+        game.state.start("GameOver");
+    }
     
 };
 
@@ -136,19 +215,75 @@ mainGameState.spawnAsteroid = function (){
 //----------------------------------------------------------------------
 
 mainGameState.spawnPlayerBullet = function() {
-    
-    var bullet = game.add.sprite(this.playerShip.x, this.playerShip.y, "player-bullet");    
-    bullet.anchor.setTo(0.5,0.5);
-    bullet.angle = -90;
-    game.physics.arcade.enable(bullet);
-    bullet.body.velocity.setTo(300,0);
+    if (this.fireTimer < 0){
+        this.fireTimer = 0.4;
+        
+        var bullet = game.add.sprite(this.playerShip.x, this.playerShip.y, 'player-bullet');    
+        bullet.anchor.setTo(0.5,0.5);
+        bullet.angle = -90;
+        
+        game.physics.arcade.enable(bullet);
+        bullet.body.velocity.setTo(300,0);
 
-    this.playerBullets.add(bullet);
+        this.playerBullets.add(bullet);
+        
+    }
+    var index = game.rnd.integerInRange(0, this.playerFireSfx.length-1);
+    this.playerFireSfx[index].play();
     
 };
 
+//------------------------------------------------------------------------
 
+mainGameState.updatePlayerBullets = function() {
+    
+//Shooting at the asteroids:     
+    
+    if(this.fireKey.isDown){
+        console.log("FIREBALLS! :D ");
+        this.spawnPlayerBullet();
+    }
+    
+    this.fireTimer -= game.time.physicsElapsed;
+    
+    for(var i = 0; i < this.playerBullets.children.length; i++){
+        if(this.playerBullets.children[i].x > (game.width + 100)){
+            this.playerBullets.children[i].destroy();
+        }
+    }
 
+};
+
+//------------------------------------------------------------------------
+
+mainGameState.asteroidBulletCollision = function(object1, object2) {
+    object1.pendingDestroy = true;
+    object2.pendingDestroy = true;
+//    emitter.start(false,500,15);
+    
+    var index = game.rnd.integerInRange(0, this.playerAsteroidSfx.length-1);
+    this.playerAsteroidSfx[index].play();
+    
+    this.playerScore += 10;
+    
+};
+
+//------------------------------------------------------------------------
+
+mainGameState.astroidPlayerCollision = function(object1, object2){
+    
+    if(object1.key.includes("asteroid")){
+        object1.pendingDestroy = true;
+//        emitter.start(false,500,15);
+    }else{
+        object2.pendingDestroy = true;
+//        emitter.start(false,500,15);
+    }
+    
+    this.playerLife -= 1; 
+    this.death.play();
+    
+};
 
 
 
